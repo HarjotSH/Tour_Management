afae741346bafad81d8c68c3039fea791c876107
Harjot Singh <harjotsingh01425@gmail.com>
2025-07-30 17:50:38 +0530

    solved login issue

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,3 +1,2 @@
-// export const BASE_URL = "http://localhost:4000/api/v1";
-export const BASE_URL = "https://tour-mangement-backend.onrender.com/api/v1";
-// export const BASE_URL = "https://smiling-ox-drawers.cyclic.app/api/v1";
+export const BASE_URL = "https://tour-management-5sqz.onrender.com/api/v1";
+
